fa8bb91683c674093462023c02d7b624b31be208
Jeffrey Macko <jmacko@solocal.com>
2019-05-27 14:16:10 +0200

    Fix tests

--- Tests/RelapseTests/RelapseTests.swift
+++ Tests/RelapseTests/RelapseTests.swift
@@ -24,38 +24,61 @@ final class RelapseTests: XCTestCase {
         XCTAssertEqual(output, expectedOutput)
     }
     
-    func testBasNumberOfArgument1() throws {
+    
+    //// BadNumberOfArgument
+    
+    func test_01_BadNumberOfArgument1() throws {
         try performTest([], "Bad number of argument\n")
     }
 
-    func testBasNumberOfArgument2() throws {
-        try performTest(["warning_relapse"], "Bad number of argument\n")
+    func test_02_BadNumberOfArgument2() throws {
+        try performTest(["test_relapse_superior_to"], "Bad number of argument\n")
     }
 
-    func testBasNumberOfArgument3() throws {
-        try performTest(["warning_relapse", "1"], "Bad number of argument\n")
+    func test_03_BadNumberOfArgument3() throws {
+        try performTest(["test_relapse_superior_to", "1"], "Bad number of argument\n")
     }
 
-    func testBasNumberOfArgument4() throws {
-        try performTest(["warning_relapse", "1", ">"], "Bad number of argument\n")
-    }
-
-    func testBasNumberOfArgument5() throws {
-        try performTest(["warning_relapse", "1", ">", ".test/ci/ci.sqlite3"], "😆 - We have added this new value.\n")
-    }
-
-    func testBasNumberOfArgument6() throws {
-        try performTest(["warning_relapse", "1", ">", ".test/ci/ci.sqlite3"], "😐 - The new value(1) is equal to the stored value.\n")
+    func test_04_BadNumberOfArgument4() throws {
+        try performTest(["test_relapse_superior_to", "1", ">"], "Bad number of argument\n")
     }
     
-    func testBasNumberOfArgument7() throws {
-        try performTest(["warning_relapse", "5", ">", ".test/ci/ci.sqlite3"], "😆 - We have update the saved value to 5.\n")
+    //// SuperiorTo
+
+    func test_05_AddSuperiorTo() throws {
+        try performTest(["test_relapse_superior_to", "1", ">", ".test/ci/ci.sqlite3"], "😆 - We have added this new value.\n")
+    }
+
+    func test_06_NoUpdateSuperiorTo() throws {
+        try performTest(["test_relapse_superior_to", "1", ">", ".test/ci/ci.sqlite3"], "😐 - The new value(1) is equal to the stored value.\n")
     }
     
-    func testBasNumberOfArgument8() throws {
-        try performTest(["warning_relapse", "1", ">", ".test/ci/ci.sqlite3"], "😫 - The test of \"1 > 5\" failed.\n")
+    func test_07_UpdateSuperiorTo() throws {
+        try performTest(["test_relapse_superior_to", "5", ">", ".test/ci/ci.sqlite3"], "😆 - We have update the saved value to 5.\n")
     }
     
+    func test_08_FailSuperiorTo() throws {
+        try performTest(["test_relapse_superior_to", "3", ">", ".test/ci/ci.sqlite3"], "😫 - The test of \"3 > 5\" failed.\n")
+    }
+
+    //// InferiorTo
+    
+    func test_09_AddInferiorTo() throws {
+        try performTest(["test_relapse_inferior_to", "25", "<", ".test/ci/ci.sqlite3"], "😆 - We have added this new value.\n")
+    }
+    
+    func test_10_NoUpdateInferiorTo() throws {
+        try performTest(["test_relapse_inferior_to", "25", "<", ".test/ci/ci.sqlite3"], "😐 - The new value(25) is equal to the stored value.\n")
+    }
+    
+    func test_11_UpdateInferiorTo() throws {
+        try performTest(["test_relapse_inferior_to", "2", "<", ".test/ci/ci.sqlite3"], "😆 - We have update the saved value to 2.\n")
+    }
+    
+    func test_12_FailInferiorTo() throws {
+        try performTest(["test_relapse_inferior_to", "23", "<", ".test/ci/ci.sqlite3"], "😫 - The test of \"23 < 2\" failed.\n")
+    }
+
     /// Returns path to the built products directory.
     var productsDirectory: URL {
       #if os(macOS)
@@ -69,13 +92,17 @@ final class RelapseTests: XCTestCase {
     }
 
     static var allTests = [
-        ("testBasNumberOfArgument1", testBasNumberOfArgument1),
-        ("testBasNumberOfArgument2", testBasNumberOfArgument2),
-        ("testBasNumberOfArgument3", testBasNumberOfArgument3),
-        ("testBasNumberOfArgument4", testBasNumberOfArgument4),
-        ("testBasNumberOfArgument5", testBasNumberOfArgument5),
-        ("testBasNumberOfArgument6", testBasNumberOfArgument6),
-        ("testBasNumberOfArgument7", testBasNumberOfArgument7),
-        ("testBasNumberOfArgument8", testBasNumberOfArgument8),
+        ("test_01_BadNumberOfArgument1", test_01_BadNumberOfArgument1),
+        ("test_02_BadNumberOfArgument2", test_02_BadNumberOfArgument2),
+        ("test_03_BadNumberOfArgument3", test_03_BadNumberOfArgument3),
+        ("test_04_BadNumberOfArgument4", test_04_BadNumberOfArgument4),
+        ("test_05_AddSuperiorTo", test_05_AddSuperiorTo),
+        ("test_06_NoUpdateSuperiorTo", test_06_NoUpdateSuperiorTo),
+        ("test_07_UpdateSuperiorTo", test_07_UpdateSuperiorTo),
+        ("test_08_FailSuperiorTo", test_08_FailSuperiorTo),
+        ("test_09_AddInferiorTo", test_09_AddInferiorTo),
+        ("test_10_NoUpdateInferiorTo", test_10_NoUpdateInferiorTo),
+        ("test_11_UpdateInferiorTo", test_11_UpdateInferiorTo),
+        ("test_12_FailInferiorTo", test_12_FailInferiorTo),
     ]
 }
